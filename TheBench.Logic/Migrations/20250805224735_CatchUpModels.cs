b498a54d36e4438735c57f2db4329261cac5d750
Kyle McFarland <kmcfarland@hey.com>
2025-08-05 15:49:39 -0700

    Update Migrations

---
+++ TheBench.Logic/Migrations/20250805224735_CatchUpModels.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBench.Logic.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatchUpModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_Email",
+                table: "UserProfiles",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_Email",
+                table: "UserProfiles");
+        }
+    }
+}
